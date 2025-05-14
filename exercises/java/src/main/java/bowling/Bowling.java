505a62994047492b92031f7defeff03a574f0668
Agaton Kjellberg <agaton.kjellberg@autotrader.co.uk>
2025-05-14 16:22:09 +0100

    refactored bowling to interrate through one index instead of 2 for STRIKE logic

--- exercises/java/src/main/java/bowling/Bowling.java
+++ exercises/java/src/main/java/bowling/Bowling.java
@@ -1,22 +1,43 @@
 package bowling;
 
+//public class Bowling {
+//    public int game(int[] rolls) {
+//        int score = 0;
+//        for (int  i = 0; i < rolls.length; i+=2 ){
+//            int frame = 0;
+//            if(rolls[i] == 10){
+//              frame += rolls[i];
+//              frame += rolls[i+1];
+//              frame += rolls[i+2];
+//              i-=1;
+//            } else if (rolls [i]+rolls[i+1] == 10) {
+//                frame += rolls[i];
+//                frame += rolls[i+1];
+//                frame += rolls[i+2];
+//            } else {
+//                frame += rolls[i];
+//                frame += rolls[i+1];
+//            }
+//            score += frame;
+//        }
+//        return score;
+//    }
+//}
+
 public class Bowling {
     public int game(int[] rolls) {
         int score = 0;
-        for (int  i = 0; i < rolls.length; i+=2 ){
+        for (int  i = 0; i < rolls.length; i++ ){
             int frame = 0;
-            if(rolls[i] == 10){
-              frame += rolls[i];
-              frame += rolls[i+1];
-              frame += rolls[i+2];
-              i-=1;
-            } else if (rolls [i]+rolls[i+1] == 10) {
-                frame += rolls[i];
-                frame += rolls[i+1];
+            frame += rolls[i];
+            frame += rolls[i+1];
+            if(rolls[i] == 10){  //STRIKE
                 frame += rolls[i+2];
+            } else if (rolls [i]+rolls[i+1] == 10) { //SPARE
+                frame += rolls[i+2];
+                i++;
             } else {
-                frame += rolls[i];
-                frame += rolls[i+1];
+            i++;
             }
             score += frame;
         }
