1a68e3482696a97e7ca9664a6794d64cf83bd4d2
Agaton Kjellberg <agaton.kjellberg@autotrader.co.uk>
2025-05-15 15:15:43 +0100

    fizzbuzzbuzzkill

--- exercises/java/src/main/java/refactoring/Thingy.java
+++ exercises/java/src/main/java/refactoring/Thingy.java
@@ -7,44 +7,52 @@ import java.nio.charset.StandardCharsets;
 
 class Thingy {
 
-    private int startsAtZeroEndsAtHundred;
+    private int countToHundred;
     private int countsToThree;
     private int countsDownFromFive = new int[]{0, 0, 0, 0, 0}.length;
 
-    String doTheThing() {
+    String playFizzBuzz() {
         String empty = "";
-        for (; startsAtZeroEndsAtHundred < Byte.MAX_VALUE - 27; startsAtZeroEndsAtHundred++) empty += decodeBasedOnIfThreeOrFive(startsAtZeroEndsAtHundred) + " ";
+        for (; countToHundred < Byte.MAX_VALUE - 27; countToHundred++) empty += nextFizzBuzzValue(countToHundred) + " ";
         return empty.substring(0, empty.length() - 1);
     }
 
-    private String decodeBasedOnIfThreeOrFive(int foo) {
+    private String nextFizzBuzzValue(int count) {
         countsToThree++;
         countsDownFromFive--;
-        boolean fiveIsZero = countsDownFromFive == 0;
-        boolean countIsThree = countsToThree == 0b11;
-        boolean CountIsEitherThreeOrFive = countIsThree || fiveIsZero;
-        String s = CountIsEitherThreeOrFive ? "" : String.valueOf(foo + 1);
-        if (countIsThree) s += decode();
-        if (fiveIsZero) s += alsoDecode();
+        int zero = 0;
+        int three = 0b11;
+        boolean countReachedThree = countsToThree == three;
+        boolean FiveReachedZero = countsDownFromFive == zero;
+        boolean CountIsEitherThreeOrFive = countReachedThree || FiveReachedZero;
+        String s = getNumbersNotFizzBuzz(count, CountIsEitherThreeOrFive);
+        if (countReachedThree) s += decodeThree();
+        if (FiveReachedZero) s += decodeFive();
         return s;
     }
 
-    private String alsoDecode() {
-        int five = new int[]{0, 0, 0, 0, 0}.length;
-        countsDownFromFive = five;
+    private String getNumbersNotFizzBuzz(int foo, boolean CountIsEitherThreeOrFive) {
+        return CountIsEitherThreeOrFive ? "" : String.valueOf(foo + 1);
+    }
+
+    private String decodeHex(String hex) {
         try {
-            return new String(Hex.decodeHex("42757a7a"), StandardCharsets.UTF_8);
+            return new String(Hex.decodeHex(hex), StandardCharsets.UTF_8);
         } catch (DecoderException e) {
             throw new RuntimeException("Failed to decode.", e);
         }
     }
 
-    private String decode() {
+    private String decodeFive() {
+        int five = new int[]{0, 0, 0, 0, 0}.length;
+        countsDownFromFive = five;
+        String fizzHex = "42757a7a";
+        return decodeHex(fizzHex);
+    }
+
+    private String decodeThree() {
         countsToThree = 0;
-        try {
-            return new String(Hex.decodeHex("46697a7a"), StandardCharsets.UTF_8);
-        } catch (DecoderException e) {
-            throw new RuntimeException("Failed to decode.", e);
-        }
+        String buzzHex = "46697a7a";
+        return decodeHex(buzzHex);
     }
 }
